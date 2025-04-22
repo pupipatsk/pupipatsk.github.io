5e46fad84516798ab6d9b589ce60acecc50cb4f3
pupipatsk <pupipat.sk@gmail.com>
2025-04-22 13:02:20 +0700

    feat: Geist Mono font

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,6 +1,6 @@
 import type React from "react";
 import type { Metadata } from "next";
-import { Libre_Caslon_Text, Geist } from "next/font/google";
+import { Libre_Caslon_Text, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
@@ -21,6 +21,13 @@ const sansSerifFont = Geist({
   variable: "--font-sans",
 });
 
+const monoFont = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+  display: "swap",
+  weight: ["300"]
+});
+
 export const metadata: Metadata = {
   title: "Pupipat Singkhorn | Portfolio",
   description:
@@ -35,7 +42,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${serifFont.variable} ${sansSerifFont.variable} font-sans`}
+        className={`${serifFont.variable} ${sansSerifFont.variable} ${monoFont.variable} font-sans`}
       >
         <ThemeProvider
           attribute="class"
